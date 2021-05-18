b935eb9cc585da2b2fb5c5986284bc7ce31a7c49
M-Ridha <harhira.mouhamedridha@gmail.com>
2021-05-18 11:09:49 +0100

    checkpoint state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react' ;
 import './App.css';
 import Pic from "./image.jpg";
+import Profile from './Profile';
 
 class App extends React.Component{
   constructor(person) {
     super(person);
 
     this.state={
-      ppic:<img src={Pic} alt ='image'/> ,
+      ppic:<img src={Pic} alt ='myimage'/> ,
       fullName:" M-Ridha Harhira ",
       profession:"A marine mechanic and a Fullstack Js Developer ",
       bio:"  I WILL EITHER FIND A WAY OR MAKE ONE ",
@@ -18,13 +19,8 @@ class App extends React.Component{
     };
   }
 
-  componentDidMount() {
-    setInterval(() => {
-      this.setState((prevState) => ({
-      time: prevState.time + 1,
-      }));
-    }, 500);
-  }
+  
+ 
 
   handleClick = () => {
     this.setState({
@@ -40,20 +36,9 @@ class App extends React.Component{
         <div className="App">
 
           <button onClick={this.handleClick}> {this.state.show? 'hide profile' : ' show profile '} </button>
-          {this.state.show? (
-
-          this.state.show && (
-        <>
-          <div className="pic" > {this.state.ppic} </div> 
-          <h1 style={{fontFamily:"cursive" }}> {this.state.fullName} </h1>  
-          <h3 style={{fontFamily:"cursive" }}> {this.state.profession} </h3>  
-          <h2 style={{fontFamily:"cursive" }}> {this.state.bio} </h2> 
-        </> 
-          ) ) :(
-            <p >The last component was mounted since: {this.state.time} seconds</p>       
-          ) }
-          
-          
+      
+          {this.state.show &&   <Profile fullName={this.state.fullName} profession={this.state.profession} bio={this.state.bio}  ppic={this.state.ppic}/>}
+       
         </div>
       );
   }
